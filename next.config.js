675aefd37a098a231556f2adcd0e3bedd9830315
0xjc65.btc <juliocesarurss65@gmail.com>
2026-05-04 00:55:44 +0200

    Implement CORS headers in next.config.js
    Add CORS headers to all routes in the Next.js config.

--- next.config.js
+++ next.config.js
@@ -7,7 +7,17 @@ const nextConfig = {
   },
   images: {
     domains: ['assets.coingecko.com', 'avatars.githubusercontent.com'],
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
